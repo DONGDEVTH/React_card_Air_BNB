886486371f67e81f68d35d7a9308730d531d9ade
DONGDEVTH <51239005+DONGDEVTH@users.noreply.github.com>
2023-02-20 11:46:38 +0700

    React_card_Air_BNB

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Top from './components/Top'
+import Info from './components/Info'
+//import Interests from './components/Interests'
+import Card from './components/Card'
+
+ //<Info />
+function App() {
+  return (
+    <div className="App">
+    <Top />
+    <Card />
+    </div>
+  );
+}
+
+export default App;
